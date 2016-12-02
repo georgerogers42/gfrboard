8f3d658155166be6d94bceacc0b343a4cc98dadd
George Rogers <grogers385@gmail.com>
2016-12-02 17:43:04 -0600

    update

--- author.sql
+++ author.sql
@@ -1,2 +1,2 @@
-alter table threads add author varchar not null default = 'anonymous coward';
-alter table posts add author varchar not null default = 'anonymous coward';
+alter table threads add author varchar not null default 'anonymous coward';
+alter table posts add author varchar not null default 'anonymous coward';
