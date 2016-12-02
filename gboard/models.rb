70dbf2ddb051943d287d1842733a8f648905b0ab
George Rogers <grogers385@gmail.com>
2016-12-02 16:33:09 -0600

    update

--- gboard/models.rb
+++ gboard/models.rb
@@ -5,15 +5,9 @@ require_relative 'urlparse'
 
 module GBoard
   module Models
-    extend self
     DB = Sequel.connect(ENV["DATABASE_URL"])
     class Board < Sequel::Model
       one_to_many :threads
-    end
-    module MDContents
-      def md_contents
-        MD.render contents
-      end
     end
     class Thread < Sequel::Model
       many_to_one :board
